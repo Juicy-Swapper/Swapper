a35cc8f2ca1e28b343319efa1539e1f115f845c9
kaede <69410676+0xkaede@users.noreply.github.com>
2021-08-01 21:35:09 +0100

    Added revert

--- JuicySwapper_V2/Main/Forms/SkinSwapper.cs
+++ JuicySwapper_V2/Main/Forms/SkinSwapper.cs
@@ -46,7 +46,56 @@ namespace JuicySwapper_V2.Main.Forms
 
         private void RevertBtn_Click(object sender, EventArgs e)
         {
+            WebClient ProgramClient = new();
 
+            dynamic parsed = JObject.Parse(ProgramClient.DownloadString("https://juicyswapper.netlify.app/api/v1/Skins.json"));
+
+            LogBox.Text += $"[LOG] Starting...\n";
+
+            foreach (var cosmetic in parsed.skins)
+            {
+                string skinsname = cosmetic.name;
+                if (skinsname.ToString().Contains(vars.item))
+                {
+                    foreach (var asset in cosmetic.assets)
+                    {
+                        string mainasset = $"{asset.parentasset}.uasset";
+
+                        LogBox.Text += $"[LOG] Getting uasset\n";
+
+                        bool exportbool = Engine.ExportCompressed(mainasset.ToString(), Directory.GetCurrentDirectory());
+                        if (exportbool)
+                            LogBox.Text += $"[LOG] Successfully Getting uasset\n";
+                        else
+                        {
+                            LogBox.Text += $"[LOG] Error Getting uasset\n";
+                            return;
+                        }
+
+                        Oodle.Oodle.Compress(mainasset, mainasset + "Non");
+
+                        var dataswap = File.ReadAllBytes(mainasset + "Non");
+
+                        var utocTucas = CUE4Parse.Kaede.PakFile.Replace("utoc", "ucas");
+
+                        bool SwapUassetBool = Researcher.SwapUasset($"D:\\Games\\Fortnite\\FortniteGame\\Content\\Paks\\{utocTucas.Replace("10", "100")}", CUE4Parse.Kaede.offset, dataswap);
+                        if (SwapUassetBool)
+                            LogBox.Text += $"[LOG] Uasset Successfully added\n";
+                        else
+                            LogBox.Text += $"[LOG] Error adding Uasset\n";
+
+                        try
+                        {
+                            PaksLocations.DeleteDirectory("FortniteGame");
+                        }
+                        catch { }
+                    }
+                    LogBox.Text += $"[LOG] Done!\n";
+
+                }
+            }
+
+                
         }
 
         private void ConvertBtn_Click(object sender, EventArgs e)
@@ -89,8 +138,6 @@ namespace JuicySwapper_V2.Main.Forms
                         Oodle.Oodle.Compress(mainasset, mainasset + ".Compressed");
 
                         var dataswap = File.ReadAllBytes(mainasset + ".Compressed");
-
-                        //MessageBox.Show(CUE4Parse.Kaede.PakFile.Replace("10", "100"));
 
                         var utocTucas = CUE4Parse.Kaede.PakFile.Replace("utoc", "ucas");
 
